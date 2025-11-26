eb5a346823a7d963ab201b76fc6672fb1bd34fbf
fanil279 <fanilramakaev@gmail.com>
2025-11-26 19:41:34 +0500

    feat: enhance EventCard layout and improve event details display

--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -11,13 +11,13 @@ import type { Event } from '../types';
 import eventsService from '../services/eventsService';
 
 const EventCard: FC = () => {
-    let eventId;
     const userId = useSelector((state: RootState) => state.auth.user?.id);
+    const { isAuthenticated } = useSelector((state: RootState) => state.auth);
 
     const [events, setEvents] = useState<Event[] | null>(null);
+    const [selectedEventId, setSelectedEventId] = useState<number | null>(null);
     const [showEventModal, setShowEventModal] = useState(false);
 
-    const { isAuthenticated } = useSelector((state: RootState) => state.auth);
     const { showNotAuthModal, setShowNotAuthModal } = useIsAuthModal();
 
     useEffect(() => {
@@ -46,12 +46,11 @@ const EventCard: FC = () => {
 
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {events?.map((event) => {
-                        eventId = event.id;
-
                         return (
                             <div
                                 key={event.id}
                                 className='
+                                    flex flex-col
                                     bg-slate-800/25
                                     border border-white/10
                                     rounded-xl 
@@ -63,35 +62,7 @@ const EventCard: FC = () => {
                                     text-white
                                 '
                             >
-                                {userId === event.organiser.id && (
-                                    <div className='flex gap-3'>
-                                        <Button
-                                            variant='tertiary'
-                                            onClick={() => {
-                                                requireAuth(() => {
-                                                    setShowEventModal(true);
-                                                }, isAuthenticated)
-                                            }}
-                                        >
-                                            Edit event
-                                        </Button>
-
-                                        {showNotAuthModal && <NotAuthModal onClose={() => setShowNotAuthModal(false)} />}
-
-                                        <Button
-                                            variant='tertiary'
-                                            onClick={() => {
-                                                requireAuth(async () => {
-                                                    await eventsService.delteEvent(event.id);
-                                                }, isAuthenticated)
-                                            }}
-                                        >
-                                            Delete event
-                                        </Button>
-                                    </div>
-                                )}
-
-                                <h3 className='text-xl font-semibold'>{event.title}</h3>
+                                <h3 className='text-xl font-semibold mt-12'>{event.title}</h3>
 
                                 <p className='text-gray-200 text-sm line-clamp-3'>{event.description}</p>
 
@@ -116,6 +87,35 @@ const EventCard: FC = () => {
                                 >
                                     Register for event
                                 </Button>
+
+                                {userId === event.organiser.id && (
+                                    <div className='flex gap-3'>
+                                        <Button
+                                            variant='tertiary'
+                                            onClick={() => {
+                                                requireAuth(() => {
+                                                    setSelectedEventId(event.id);
+                                                    setShowEventModal(true);
+                                                }, isAuthenticated);
+                                            }}
+                                        >
+                                            Edit event
+                                        </Button>
+
+                                        <Button
+                                            variant='tertiary'
+                                            onClick={() => {
+                                                requireAuth(async () => {
+                                                    await eventsService.delteEvent(event.id);
+                                                }, isAuthenticated)
+                                            }}
+                                        >
+                                            Delete event
+                                        </Button>
+                                    </div>
+                                )}
+
+                                {showNotAuthModal && <NotAuthModal onClose={() => setShowNotAuthModal(false)} />}
                             </div>
                         );
                     })}
@@ -126,7 +126,7 @@ const EventCard: FC = () => {
                 <EventModal
                     onClose={() => setShowEventModal(false)}
                     action={() => true}
-                    eventId={eventId}
+                    eventId={selectedEventId!}
                 />
             )}
         </div>
